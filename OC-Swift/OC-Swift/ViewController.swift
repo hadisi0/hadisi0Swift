aff011fa9c78b11ef497a80e82d3a3d307313557
xiaozhong <xiaozhong@xiaozhongdembp.lan>
2017-06-02 14:37:52 +0800

    我的提交

---
+++ OC-Swift/OC-Swift/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  OC-Swift
+//
+//  Created by xiaozhong on 2017/6/2.
+//  Copyright © 2017年 xiaozhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+        DispatchQueue.global().async {
+            
+            DispatchQueue.main.async(execute: { 
+                
+            })
+        }
+    
+//        我刚刚提交的东西
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
